2da045c0c59095432e1cce44db1a036e27602957
subh <subhajitchaudhury05@gmail.com>
2026-03-22 12:30:22 +0530

    formatted lsp config
    leader mp fixed for formatting specific file

--- nvim/lua/subh/plugins/lsp/lspconfig.lua
+++ nvim/lua/subh/plugins/lsp/lspconfig.lua
@@ -135,20 +135,21 @@ return {
           })
         end,
         ["rust_analyzer"] = function()
-  lspconfig["rust_analyzer"].setup({
-    capabilities = capabilities,
-    settings = {
-      ["rust-analyzer"] = {
-        cargo      = { features = "all" },
-        checkOnSave = true,
-        check      = { command = "clippy" },
-        imports    = { group = { enable = false } },
-        completion = { postfix = { enable = false } },
-      },
-    },
-  })
-end,
+          lspconfig["rust_analyzer"].setup({
+            capabilities = capabilities,
+            settings = {
+              ["rust-analyzer"] = {
+                cargo = { features = "all" },
+                checkOnSave = true,
+                check = { command = "clippy" },
+                imports = { group = { enable = false } },
+                completion = { postfix = { enable = false } },
+              },
+            },
+          })
+        end,
       },
     })
   end,
 }
+
